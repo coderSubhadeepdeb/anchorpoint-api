4fd63959c4af4ac2d8dd8775a0484e8eadced004
coderSubhadeepdeb <sbhdpdeb@gmail.com>
2025-06-29 17:59:34 +0530

    new route added

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -5,6 +5,13 @@ const router = Router();
 
 router.route("/login").post(loginAdmin);
 router.route("/logout").post(verifyToken, logoutAdmin);
+router.route("/verify-token").get(verifyToken, (req, res) => {
+    return res.status(200).json({
+        success: true,
+        message: "Token is valid",
+        admin: req.admin
+     });
+});
 router.route("/refresh-access-token").post(refreshAccessToken);
 
 export default router;
